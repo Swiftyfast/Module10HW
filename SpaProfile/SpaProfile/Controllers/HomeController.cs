994ea915a47d351949d8d5258d3f5c78df4575c8
John Swift <jdavidswift@gmail.com>
2020-12-06 16:58:40 -0600

    Working, but not pretty

---
+++ SpaProfile/SpaProfile/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SpaProfile.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace SpaProfile.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
+        {
+            string body = $"You have recevied an email from {cvm.Name}. The email address was {cvm.Email}. The following message was sent: {cvm.Message}";
+
+            MailMessage mm = new MailMessage();
+            mm.From = new MailAddress("no-reply@johndavidswift.com");
+            mm.To.Add(new MailAddress("jdavidswift@gmail.com"));
+            mm.Subject = cvm.Subject;
+            mm.Body = body;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient smtp = new SmtpClient("mail.johndavidswift.com");
+            NetworkCredential cred = new NetworkCredential("no-reply@johndavidswift.com", "");
+
+            smtp.Credentials = cred;
+            smtp.Send(mm);
+
+            return Json(cvm);
+        }
+    }
+}
